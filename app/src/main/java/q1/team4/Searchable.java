22d18724712f2cbea6bc9e6e27cbb5c38a8516a2
Evan Philakhong <evanthegamer2004@gmail.com>
2025-04-18 12:53:25 -0400

    feat: implements Searchable interface

---
+++ app/src/main/java/q1/team4/Searchable.java
@@ -0,0 +1,13 @@
+package q1.team4;
+
+public interface Searchable {
+    /**
+     * Get the unique ID of this item.
+     */
+    String getId();
+
+    /**
+     * Get the content that will be searched.
+     */
+    String getSearchableContent();
+}
