175010154fb7d92b040a832c1c83fc4e5c0690bf
OliviaxJames <oxjames251@gmail.com>
2025-04-21 16:04:15 +0000

    feat: update profile file

--- app/src/main/java/q1/team4/Profile.java
+++ app/src/main/java/q1/team4/Profile.java
@@ -34,28 +34,8 @@ public class Profile {
     public String getUserName() {
         return this.userName;
     }
+
     public Boolean getIsActive() {
         return isActive;
     }
-    /**
-     * Gets all of the Profiles boards.
-     * 
-     * @return A ArrayList of all od the Boards that have this Picture
-     */
-    // To Do
-    /* public List<String> getBoards() {
-        return new ArrayList<>(boards);
-    } */
-    /**
-     * Gets all of the Profiles boardIds.
-     * 
-     * @return A ArrayList of all od the Boards that have this Picture
-     */
-    public List<String> getBoardIds() {
-        return new ArrayList<>();
-    }
-    public Boolean getIsActive() {
-        return isActive;
-    }
-main
 }
