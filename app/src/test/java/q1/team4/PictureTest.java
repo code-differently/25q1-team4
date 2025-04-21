4a3afa7d788332751eec8dece53ad3fe05ff16d2
Evan Philakhong <evanthegamer2004@gmail.com>
2025-04-21 14:32:51 +0000

    test: implements tests for Picture class

--- app/src/test/java/q1/team4/PictureTest.java
+++ app/src/test/java/q1/team4/PictureTest.java
@@ -1,23 +1,61 @@
 package q1.team4;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PictureTest {
     private Picture picture1;
-    private final String PICTURE_ID = "test-picture-id";
-    private final String PICTURE_TITLE = "Test Picture";
+    private String title = "Test Picture";
     private final String PICTURE_DATE_ADDED = "2025-01-01";
-    private final String[] PICTURE_TAGS = {"traditional", "digital", "anime"};
+    private List<String> tags = new ArrayList<String>();
 
     @BeforeEach
     public void setUp() {
-        picture1 = new Picture(PICTURE_ID, PICTURE_TITLE, PICTURE_DATE_ADDED, PICTURE_TAGS);
+        picture1 = new Picture(title, PICTURE_DATE_ADDED, tags);
+        tags.add("traditional");
+        tags.add("digital");
+        tags.add("anime");
+    }
+
+    @Test
+    public void testAddTag() {
+        picture1.addTag("illustration");
+
+        List<String> updatedTags = picture1.getTags();
+        assertTrue(updatedTags.contains("illustration"));
+        assertEquals(4, updatedTags.size());
+    }
+
+    @Test
+    public void testRemoveTag() {
+        picture1.removeTag("anime");
+
+        List<String> updatedTags = picture1.getTags();
+        assertFalse(updatedTags.contains("anime"));
+        assertEquals(2, updatedTags.size());
+    }
+
+    @Test
+    public void testGetTitle() {
+        assertEquals(title, picture1.getTitle());
+    }
+
+    @Test
+    public void testSetTitle() {
+        String newTitle = "Updated Title";
+        picture1.setTitle(newTitle);
+        assertEquals(newTitle, picture1.getTitle());
     }
 
     @Test
     public void testGetTags() {
-        assertArrayEquals(PICTURE_TAGS, picture1.getTags());
+        assertEquals(tags, picture1.getTags());
     }
 }
