ef3892e3b68a61ed71a32216b11e327c0de1c738
Evan Philakhong <evanthegamer2004@gmail.com>
2025-04-21 03:36:21 +0000

    feat: adds Picture and Profile Class & Test

--- app/src/main/java/q1/team4/Picture.java
+++ app/src/main/java/q1/team4/Picture.java
@@ -3,55 +3,38 @@ package q1.team4;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Picture implements Searchable {
-    private final String id;
-    private final String filePath;
-    private final String title;
-    private final String[] tags;
-    private final List<String> boardIds;
+public class Picture {
+    private final String ID;
+    private final String TITLE;
+    private final String[] TAGS;
+    private final List<String> BOARD_IDS;
 
-    public Picture(String id, String filePath, String title, String[] tags) {
-        this.id = id;
-        this.filePath = filePath;
-        this.title = title;
-        this.tags = tags;
-        this.boardIds = new ArrayList<>();
+    public Picture(String ID, String TITLE, String[] TAGS) {
+        this.ID = ID;
+        this.TITLE = TITLE;
+        this.TAGS = TAGS;
+        this.BOARD_IDS = new ArrayList<>();
     }
 
     public void addToBoard(String boardId) {
-        if (!boardIds.contains(boardId)) {
-            boardIds.add(boardId);
+        if (!BOARD_IDS.contains(boardId)) {
+            BOARD_IDS.add(boardId);
         }
     }
 
     public void removeFromBoard(String boardId) {
-        boardIds.remove(boardId);
+        BOARD_IDS.remove(boardId);
     }
 
-    public String getFilePath() {
-        return this.filePath;
+    public String getId() {
+        return this.ID;
     }
 
     public String getTitle() {
-        return this.title;
+        return this.TITLE;
     }
 
     public String[] getTags() {
-        return this.tags.clone();
-    }
-
-    @Override
-    public String getId() {
-        return this.id;
-    }
-
-    @Override
-    public String getSearchableContent() {
-        StringBuilder content = new StringBuilder();
-        content.append(title).append(" ");
-        for (String tag : tags) {
-            content.append(tag).append(" ");
-        }
-        return content.toString();
+        return this.TAGS.clone();
     }
 }
