c5dad31d4abe5a398d72368043af7316fa451fdc
Evan Philakhong <evanthegamer2004@gmail.com>
2025-04-21 03:36:21 +0000

    feat: adds Picture and Profile Class & Test

---
+++ app/src/test/java/q1/team4/PictureTest.java
@@ -0,0 +1,22 @@
+package q1.team4;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PictureTest {
+    private Picture picture1;
+    private final String PICTURE_ID = "test-picture-id";
+    private final String PICTURE_TITLE = "Test Picture";
+    private final String[] PICTURE_TAGS = {"traditional", "digital", "anime"};
+
+    @BeforeEach
+    public void setUp() {
+        picture1 = new Picture(PICTURE_ID, PICTURE_TITLE, PICTURE_TAGS);
+    }
+
+    @Test
+    public void testGetTags() {
+        assertArrayEquals(PICTURE_TAGS, picture1.getTags());
+    }
+}
