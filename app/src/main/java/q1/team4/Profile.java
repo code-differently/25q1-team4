48fc0487072afab05d6a25cd87de3c66fb6274ab
Evan Philakhong <evanthegamer2004@gmail.com>
2025-04-21 13:20:14 +0000

    fix: removed random text and getBoardIds method

--- app/src/main/java/q1/team4/Profile.java
+++ app/src/main/java/q1/team4/Profile.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 public class Profile {
     private final String id;
     private final String userName;
-UserFaceSettings
     private final List<Board> boards;
     private final Boolean isActive;
     /**
@@ -35,25 +34,8 @@ UserFaceSettings
     public String getUserName() {
         return this.userName;
     }
-UserFaceSettings
+    
     public Boolean getIsActive() {
         return isActive;
-    }
-    /**
-     * Gets all of the Profiles boards.
-     * 
-     * @return A ArrayList of all od the Boards that have this Picture
-     */
-    // To Do
-    /* public List<String> getBoards() {
-        return new ArrayList<>(boards);
-    } */
-    /**
-     * Gets all of the Profiles boardIds.
-     * 
-     * @return A ArrayList of all od the Boards that have this Picture
-     */
-    public List<String> getBoardIds() {
-        return new ArrayList<>(boardIds);
     }
 }
