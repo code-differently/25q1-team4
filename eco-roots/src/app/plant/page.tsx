b666a7186d667f4e0b27121d2aa4b886f3fe0718
Tezz03 <mmbradley251@gmail.com>
2025-05-19 19:59:18 +0000

    fix: delted file page.tsx

--- eco-roots/src/app/plant/page.tsx
+++
@@ -1,9 +0,0 @@
-import PlantForm from "@/components/plant-form"
-
-export default function PlantPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <PlantForm />
-    </div>
-  )
-}
