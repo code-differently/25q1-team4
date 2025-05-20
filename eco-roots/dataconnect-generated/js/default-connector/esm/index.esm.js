2f40118faa5ffa2642ac91315dc37cfdf9f36392
OliviaxJames <oxjames251@gmail.com>
2025-05-20 16:38:11 -0400

    chore: moves all eco-roots files to branchout

--- eco-roots/dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'eco-roots',
-  location: 'us-central1'
-};
-
--- eco-roots/dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,7 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'eco-roots',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
